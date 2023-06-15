ccc55789ad7147d09c54c6c4fb304f40356452ae
Babatunde Jubril <babatundejubril@gmail.com>
2023-06-15 00:25:00 -0700

    Done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -10,3 +10,4 @@ exec { 'increase-hard-file-limit-for-holberton-user':
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
+}
